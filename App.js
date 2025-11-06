411311b11817764615310331f07d1d8164b78a7e
Adryan <adryan.campos@escola.pr.gov.br>
2025-11-06 12:54:42 +0000

    a

--- App.js
+++ App.js
@@ -2,19 +2,46 @@ import React from "react";
 import { ScrollView } from 'react-native';
 import Titulo from "./componentes/Titulo";
 import Saudacao from "./componentes/Saudacao";
+import FotoPerfil from "./componentes/FotoPerfil";
+import CartaoUsuario from "./componentes/CartaoUsuario";
 
 export default function App(){
-  return(
-    <ScrollView style={{ padding:20}}>
+  return <ScrollView>
     <Titulo />
-    <Saudacao nome="plk"/>
-    <Saudacao nome="plk"/>
-    <Saudacao nome="plk"/>
+    <Saudacao nome='Plk' />
+    <FotoPerfil />
 
+    <CartaoUsuario 
+    nome='Plk'
+    descricao='progamador'
+    img='perfil00'
+    />
 
+    <CartaoUsuario 
+    nome='Robinho'
+    descricao='analisador de dados'
+    img='perfil01'
+    />
 
+    <CartaoUsuario 
+    nome='Robinho'
+    descricao='segurança de dados'
+    img='perfil02'
+    />
 
+    <CartaoUsuario 
+    nome='Patinhas'
+    descricao='Analisador de codigos'
+    img='perfil03'
+    />
+
+    <CartaoUsuario 
+    nome='Donalt'
+    descricao='progamador'
+    img='perfil04'
+    />
+    
     </ScrollView>
 
-  )
+ 
 }
